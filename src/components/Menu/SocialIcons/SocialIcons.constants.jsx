f55095591a9ba386d227d6b61326e2ee69659062
AntonRomanenkov <romanenkovanton17@gmail.com>
2023-02-01 13:09:29 +0900

    Adjust link

--- src/components/Menu/SocialIcons/SocialIcons.constants.jsx
+++ src/components/Menu/SocialIcons/SocialIcons.constants.jsx
@@ -1,9 +1,9 @@
 // import cv from "./CV.pdf";
 
 export const socialLinks = [
-	{ name: 'Twitter', link: 'https://twitter.com/_Eduard26' },
-	{ name: 'Dribbble', link: 'https://dribbble.com/Eduard26' },
-	{ name: 'Github', link: 'https://github.com/eduardconstantin' },
-	// {name: "Resume", link: cv},
-	{ name: 'Mail', link: 'mailto: ibinceanu.eduard@yahoo.com' },
+	{ name: 'Fiverr', link: 'https://www.fiverr.com/users/romanenkovanton' },
+	{ name: 'Freelancer', link: 'https://www.freelancer.com/u/romanenkovanton?w=f' },
+	{ name: 'Linkedin', link: 'https://www.linkedin.com/in/anton-romanenkov-a78470249/' },
+	{ name: 'Github', link: 'https://github.com/AntonRomanenkov' },
+	{ name: 'Mail', link: 'mailto: romanenkovanton17@gmail.com' },
 ];
